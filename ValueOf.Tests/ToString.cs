4b42170de079a0fca535656a5768319adb587bf4
Shaw, Polly <Polly.Shaw@uk.tesco.com>
2017-08-25 21:39:19 +0100

    Implemented ToString method in ValueOf to return ToString of Value, because I have created a few bugs this afternoon by forgetting to append `.Value` when calling ToString implicitly.

---
+++ ValueOf.Tests/ToString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ValueOf.Tests
+{
+    public class ToString
+    {
+        [Test]
+        public void ToStringReturnsValueToStringForSingleValuedObjects()
+        {
+            ClientRef clientRef1 = ClientRef.From("ASDF12345");
+            
+            Assert.AreEqual(clientRef1.Value, clientRef1.ToString());
+        }
+
+        [Test]
+        public void ToStringReturnsValueOfTupleForTupleValuedObjects()
+        {
+            Address address1 = Address.From(("16 Food Street", "London", Postcode.From("N1 1LT")));
+            
+            Assert.AreEqual(address1.Value.ToString(), address1.ToString());
+
+        }
+    }
+}
